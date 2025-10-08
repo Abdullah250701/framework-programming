2ff86f413c316eef9a4bb4218c8d4545f283a01a
abdullah <adibinsyarif6@gmail.com>
2025-10-08 23:09:51 +0700

    Selesai Modul 2: Relasi Model Warga dan Pengaduan

--- data_kelurahan/warga/views.py
+++ data_kelurahan/warga/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Warga
-
+from .models import Warga, Pengaduan
 
 class WargaListView(ListView):
     model = Warga
+
+class PengaduanListView(ListView):  
+    model = Pengaduan
+    template_name = 'warga/pengaduan_list.html'
